5a2081ee36a6724fdd3fb3086b43d5f97cbcf3fd
Мария Лепина <mv.kashina@mail.ru>
2026-01-05 12:36:55 +0500

    add function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,19 @@
 import re
-
-def get_number(type_number: str) -> int:
-    """Очистка номера счета/карты"""
-    list_numbers = re.findall(r'\d+', type_number)
-    number = int(list_numbers[0])
-    return number
-
+import masks
 
 def mask_account_card(type_number: str) -> str:
     """Возвращает строку с замаскированным номером карты или счета"""
-    pass
-    return something
+    list_numbers = re.findall(r'\d+', type_number)
+    number = int(list_numbers[0])
+    if len(list_numbers[0]) == 16:
+        result = masks.get_mask_card_number(number)
+    elif len(list_numbers[0]) == 20:
+        result = masks.get_mask_account(number)
+    else:
+        result = "Проверьте корректность входных данных"
+    return result
 
-print(get_number("Visa Platinum 7000792289606361"))
-print(get_number("Maestro 7000792289606361"))
-print(get_number("Счет 73654108430135874305"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Счет 784304305"))
