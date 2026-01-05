040ed7cd5e82638e6e7322d8891534eb446957f6
Мария Лепина <mv.kashina@mail.ru>
2026-01-05 13:59:07 +0500

    function get_date

--- src/widget.py
+++ src/widget.py
@@ -14,8 +14,19 @@ def mask_account_card(type_number: str) -> str:
         result = "Проверьте корректность входных данных"
     return result
 
+
+def get_date(date: str) -> str:
+    date_pattern = r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})"
+    match = re.search(date_pattern, date)
+    if match:
+        formated_date = match.group('day') + "." + match.group('month') + "." + match.group('year')
+    return formated_date
+
+
 # Проверка:
-print(mask_account_card("Visa Platinum 7000792289606361"))
-print(mask_account_card("Maestro 7000792289606361"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(mask_account_card("Счет 784304305"))
+# print(mask_account_card("Visa Platinum 7000792289606361"))
+# print(mask_account_card("Maestro 7000792289606361"))
+# print(mask_account_card("Счет 73654108430135874305"))
+# print(mask_account_card("Счет 784304305"))
+
+# print(get_date("2024-03-11T02:26:18.671407"))
