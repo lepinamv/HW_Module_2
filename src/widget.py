fb770547433714eef8ad4b55ecbc88f5b5a4e890
Мария Лепина <mv.kashina@mail.ru>
2026-01-05 14:17:12 +0500

    clear code for verification

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,7 @@
 import re
+
 import masks
+
 
 def mask_account_card(type_number: str) -> str:
     """Возвращает строку с замаскированным номером карты или счета"""
@@ -16,6 +18,7 @@ def mask_account_card(type_number: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """Меняет формат даты на DD.MM.YYYY"""
     date_pattern = r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})"
     match = re.search(date_pattern, date)
     if match:
@@ -24,9 +27,15 @@ def get_date(date: str) -> str:
 
 
 # Проверка:
-# print(mask_account_card("Visa Platinum 7000792289606361"))
-# print(mask_account_card("Maestro 7000792289606361"))
-# print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
+
 # print(mask_account_card("Счет 784304305"))
 
-# print(get_date("2024-03-11T02:26:18.671407"))
+print(get_date("2024-03-11T02:26:18.671407"))
