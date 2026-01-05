96c940af56573bb39812c7bb309aece2cd9dfa94
Мария Лепина <mv.kashina@mail.ru>
2026-01-05 13:19:41 +0500

    mask_account_card return type and mask

--- src/widget.py
+++ src/widget.py
@@ -4,15 +4,17 @@ import masks
 def mask_account_card(type_number: str) -> str:
     """Возвращает строку с замаскированным номером карты или счета"""
     list_numbers = re.findall(r'\d+', type_number)
+    list_type = re.findall(r'\b[a-zA-Zа-яА-Я]+\b', type_number)
     number = int(list_numbers[0])
     if len(list_numbers[0]) == 16:
-        result = masks.get_mask_card_number(number)
+        result = list_type[0] + " " + masks.get_mask_card_number(number)
     elif len(list_numbers[0]) == 20:
-        result = masks.get_mask_account(number)
+        result = list_type[0] + " " + masks.get_mask_account(number)
     else:
         result = "Проверьте корректность входных данных"
     return result
 
+# Проверка:
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Maestro 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
